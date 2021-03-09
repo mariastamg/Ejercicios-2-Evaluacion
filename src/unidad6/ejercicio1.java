eb5f51615b97c3bcce36878916cce7cbbd89bba5
Maria Solar <mariasolar8@gmail.com>
2021-03-09 21:10:58 +0100

    Ejercicio 1 de la unidad 6 resuelto

---
+++ src/unidad6/ejercicio1.java
@@ -0,0 +1,36 @@
+package unidad6;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(isInt("4"));
+		System.out.println(isInt("4,50"));
+
+	}
+
+	public static boolean isInt(String n) {
+
+		boolean respuesta;
+		try {
+			Integer.parseInt(n);
+			respuesta = true;
+		} catch (NumberFormatException exception) {
+			respuesta = false;
+		}
+		return respuesta;
+	}
+	
+	public static boolean isDouble(String n) {
+		
+		boolean respuesta;
+		try {
+			Double.parseDouble(n);
+			respuesta=true;
+		}catch (NumberFormatException exception) {
+			respuesta=false;
+		}
+		return respuesta;
+	}
+
+}
